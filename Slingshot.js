443d3466048ce0af8d774dcb17d7849771fab0cf
Saalim Khan <68114437+saalimkhan13@users.noreply.github.com>
2020-10-10 01:53:21 +0530

    Update Slingshot.js

--- Slingshot.js
+++ Slingshot.js
@@ -22,8 +22,8 @@ class SlingShot{
     }
 
     display(){
-        image(this.sling1,200,20);
-        image(this.sling2,170,20);
+        image(this.sling1,200,200,20);
+        image(this.sling2,100,170,20);
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
